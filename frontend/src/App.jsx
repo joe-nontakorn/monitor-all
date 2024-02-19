853647a3245ef77ab39fce106ab3bb2d357d41ef
joe-nontakorn <nontakorn.joe01@gmail.com>
2024-02-19 15:52:29 +0700

    update vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Test from './components/test'; // Changed import statement to use uppercase 'Test'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Test /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/test.jsx
@@ -0,0 +1,10 @@
+function test() {
+    return (
+      <div>
+          <h1>test</h1>
+      </div>
+    )
+  }
+  
+  export default test
+  
