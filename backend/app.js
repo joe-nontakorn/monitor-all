a4ad6d1c31163209eb370bbbb29b641fd06857b1
Nontakorn Khnaphol <125560220+joe-nontakorn@users.noreply.github.com>
2024-03-08 08:43:02 +0700

    no message

--- backend/app.js
+++ backend/app.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
     }
 });
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use(cors());
 
